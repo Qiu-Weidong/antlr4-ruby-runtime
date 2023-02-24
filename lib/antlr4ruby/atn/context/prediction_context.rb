f8497b2fbfb69feb84973e7a869369df377276da
unknown <568385476@qq.com>
2023-02-24 09:31:27 +0800

    ..

--- lib/antlr4ruby/atn/context/prediction_context.rb
+++ lib/antlr4ruby/atn/context/prediction_context.rb
@@ -4,6 +4,8 @@ module Antlr4ruby
 
     def initialize(cached_hash_code)
       @cached_hash_code = cached_hash_code
+      @id = @@global_node_count
+      @@global_node_count += 1
     end
 
     private
@@ -15,10 +17,6 @@ module Antlr4ruby
 
     EMPTY_RETURN_STATE = 2147483647
     attr_reader :id, :cached_hash_code
-
-    def self.from_rule_context(atn, outer_context)
-      # todo
-    end
 
     def size
       raise NotImplementedError.new("#{self.class.name}#size is abstract method")
@@ -48,7 +46,56 @@ module Antlr4ruby
       raise NotImplementedError.new("#{self.class.name}#eql? is abstract method")
     end
 
+    def to_s(recognizer)
+      super()
+    end
 
+    class << self
+      def from_rule_context(atn, outer_context)
+        # todo
+      end
+
+      def merge(a, b, root_is_wildcard, merge_cache)
+        # todo
+      end
+
+      def merge_singletons(a, b, root_is_wildcard, merge_cache)
+        # todo
+      end
+
+      def merge_root(a, b, root_is_wildcard)
+        # todo
+      end
+
+      def merge_arrays(a, b, root_is_wildcard, merge_cache)
+        # todo
+      end
+
+      def to_dot_string(context)
+        # todo
+      end
+
+      def get_cached_context(context, context_cache, visited)
+        # todo
+      end
+
+
+      protected
+      def calculate_empty_hash_code
+        # todo
+      end
+
+      def calculate_hash_code(parent: nil, return_state: 0, parents: [], return_states: [])
+        # todo
+      end
+
+      def combine_common_parents(parents)
+        # todo
+      end
+
+
+
+    end
 
 
   end
---
+++ lib/antlr4ruby/misc/double_key_map.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+
+
+module Antlr4ruby
+  class DoubleKeyMap
+
+    private
+    attr_accessor :data
+
+    public
+    def put(key1, key2, value)
+      data[key1] = {} unless data.has_key?(key1)
+      sub_data = data[key1]
+      prev = sub_data[key2]
+      sub_data[key2] = value
+      prev
+    end
+
+    def get(key1, key2)
+      sub_data = data[key1]
+      return nil unless sub_data
+      sub_data[key2]
+    end
+
+
+  end
+end
